4f9216f850b4031e7115b10bb141033a1eb893ca
Cameron Stanislawski-Doyle <cameron.stanislawski-doyle@ntlworld.com>
2022-02-24 14:58:34 +0000

    Added check for output correctness

--- code_timing/runtime_timer.py
+++ code_timing/runtime_timer.py
@@ -1,3 +1,4 @@
+from cgi import test
 import subprocess
 import os
 from timeit import timeit
@@ -41,15 +42,30 @@ def change_nx_value(old_val: int, new_val: int):
     with open(f90_file, "w") as f:
         f.write(full_file)
 
+def is_output_correct(correct_code_cmd, test_code_cmd):
+    print(f"Testing code output. Calling {correct_code_cmd} and {test_code_cmd}")
+    proc = subprocess.run(correct_code_cmd, stdout=subprocess.PIPE)
+    correct_output = proc.stdout
+
+    proc = subprocess.run(test_code_cmd, stdout=subprocess.PIPE)
+    test_output = proc.stdout
+
+    print("Comparing output...")
+    return correct_output == test_output
+
 
 if os.path.isdir("code_timing"):
     os.chdir("code_timing")
 
 f90_file = os.path.join("..", "2D_compressible.f90")
-
 if not os.path.exists(f90_file):
     print(f"Error: Fortran file not found: {f90_file}")
     quit(1)
+
+original_f90_file = "original_2D_compressible.f90"
+if not os.path.exists(original_f90_file):
+    print("Warning: Original code not found")
+    original_f90_file = None
 
 filename = os.path.join("execution_timings.xlsx")
 
@@ -58,14 +74,10 @@ workbook, timings_sheet = setup_worksheet(filename)
 compiler_cmds = [
     ["gfortran -O3", "gfortran -O3"],
     ["gfortran -O3 -fopenmp", "gfortran -O3 -fopenmp"],
+    ["gfortran -O3 -fopenmp -ftree-parallelize-loops=1", "gfortran -O3 -fopenmp 1 threads"],
     ["gfortran -O3 -fopenmp -ftree-parallelize-loops=2", "gfortran -O3 -fopenmp 2 threads"],
-    ["gfortran -O3 -fopenmp -ftree-parallelize-loops=4", "gfortran -O3 -fopenmp 4 threads"],
-    ["gfortran -O3 -fopenmp -ftree-parallelize-loops=6", "gfortran -O3 -fopenmp 6 threads"],
-    ["gfortran -O3 -fopenmp -ftree-parallelize-loops=12", "gfortran -O3 -fopenmp 12 threads"]
-    # ["gfortran -O3 -ftree-parallelize-loops=2", "gfortran -O3 2 threads"],
-    # ["gfortran -O3 -ftree-parallelize-loops=4", "gfortran -O3 4 threads"],
-    # ["gfortran -O3 -ftree-parallelize-loops=6", "gfortran -O3 6 threads"],
-    # ["gfortran -O3 -ftree-parallelize-loops=12", "gfortran -O3 12 threads"]
+    ["gfortran -O3 -fopenmp -ftree-parallelize-loops=3", "gfortran -O3 -fopenmp 3 threads"],
+    ["gfortran -O3 -fopenmp -ftree-parallelize-loops=4", "gfortran -O3 -fopenmp 4 threads"]
     ]
 
 mesh_nx_options = [129, 257, 513, 1025, 2049]
@@ -96,9 +108,10 @@ for nx_option_idx in range(len(mesh_nx_options)):
             compile_cmd = cmd
         else:
             compile_cmd = cmd + " -o output.exe " + f90_file
+            original_code_cmd = cmd + " -o correct_output.exe " + original_f90_file
 
         print("\nCompiling using: " + compile_cmd)
-        subprocess.run(compile_cmd.split(), stdout=subprocess.STDOUT, stderr=subprocess.STDOUT)
+        subprocess.run(compile_cmd.split(), stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
         if os.path.exists("output.exe"):
             print("Successfully compiled. Running output.exe")
@@ -106,6 +119,20 @@ for nx_option_idx in range(len(mesh_nx_options)):
             print("Compile failed. Moving to next compiler command.")
             next_excel_col += 1
             continue
+
+        if mesh_nx_options[nx_option_idx] == 129:
+            print("Compiling original code for comparison.")
+            subprocess.run(original_code_cmd.split(), stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+            is_correct = is_output_correct("./correct_output.exe", "./output.exe")
+            os.remove("./correct_output.exe")
+
+            if is_correct:
+                print("Code output is correct. Measuring runtime.")
+            else:
+                print("Incorrect output file produced. Moving to next compiler command.")
+                save_to_worksheet(timings_sheet, next_excel_col, "N/A")
+                next_excel_col += 1
+                continue
 
         elapsed_time = timeit(
             stmt = "subprocess.run('./output.exe', stdout=subprocess.DEVNULL)",
